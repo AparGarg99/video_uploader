61596e32bda2cf6c44696ca99cc81512affc651c
shantamshrestha <shantamshrestha@gmail.com>
2024-02-03 19:33:19 +0545

    added new logic

--- youtube2.py
+++ youtube2.py
@@ -1,4 +1,6 @@
 import os
+from urllib.parse import parse_qs, urlparse
+import uuid
 #CURRENT_FOLDER = r'C:\Users\aparg\Desktop\opraahfx'
 # CURRENT_FOLDER = r'C:\Users\apar\Desktop\oprahfx'
 CURRENT_FOLDER = './'
@@ -10,6 +12,7 @@ from pydrive.drive import GoogleDrive
 from pynput.keyboard import Key, Controller
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 import undetected_chromedriver as uc
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -17,7 +20,11 @@ from tqdm import tqdm
 import pandas as pd
 import time
 import random
-
+import psycopg2
+from psycopg2 import pool
+from sqlalchemy import create_engine
+from contextlib import contextmanager
+from datetime import datetime
 #%%
 
 #################### User input ####################
@@ -41,7 +48,177 @@ PATH_DICT = {
     }
 
 #%%
+db_params = {
+    'host': 'opraah-database.c9qouuiwyuwx.ap-south-1.rds.amazonaws.com',
+    'database': 'opraah',
+    'user': 'postgres',
+    'password': 'VUFPZaluUQk',
+    'port': '5432'
+}
 
+max_connections = 1
+
+# Create a connection pool
+connection_pool = psycopg2.pool.ThreadedConnectionPool(
+    minconn=1,
+    maxconn=max_connections,
+    **db_params
+)
+
+@contextmanager
+def connect_to_database():
+    connection = connection_pool.getconn()
+
+    try:
+        yield connection  # Provide the connection to the caller
+    finally:
+        connection_pool.putconn(connection)  # Release the connection back to the pool
+
+def get_and_update_user():
+    try:
+        # Connect to the database using the context manager
+        with connect_to_database() as connection:
+            # Create a cursor to interact with the database
+            with connection.cursor() as cursor:
+                # SQL query to select a user with less than three videos uploaded
+                select_query = """
+                SELECT email, password, videos_uploaded
+                FROM public.accounts
+                WHERE videos_uploaded < 3
+                AND
+                in_use = False
+                ORDER BY last_used ASC
+                LIMIT 1
+                FOR UPDATE SKIP LOCKED;
+                """
+
+                # Execute the SELECT query
+                cursor.execute(select_query)
+
+                # Fetch the selected user
+                user = cursor.fetchone()
+
+                if user:
+                    # Extract user information
+                    email, password, videos_uploaded = user
+
+                    # Update the in_use value and last_used timestamp
+                    update_query = """
+                    UPDATE public.accounts
+                    SET in_use = true, last_used = %s
+                    WHERE email = %s;
+                    """
+
+                    # Execute the UPDATE query
+                    cursor.execute(update_query, (datetime.now(), email))
+
+                    # Commit the changes to the database
+                    connection.commit()
+
+                    print(f"User {email} is now in use.")
+
+                    # Return information about the updated user
+                    return {'email': email, 'password': password, 'videos_uploaded': videos_uploaded}
+
+                else:
+                    print("No eligible user found.")
+                    return None
+
+    except Exception as e:
+        # Handle any exceptions
+        print(f"Error: {e}")
+        return None
+
+def fetch_video():
+    try:
+        # Connect to the database using the context manager
+        with connect_to_database() as connection:
+            # Create a cursor to interact with the database
+            with connection.cursor() as cursor:
+                # SQL query to fetch one record with status 'NOT PROCESSED' from video_metadata table
+                select_query = """
+                SELECT * FROM public.video_metadata WHERE is_processed = 'NOT PROCESSED' LIMIT 1;
+                """
+
+                # Execute the SELECT query
+                cursor.execute(select_query)
+
+                # Fetch one record
+                record = cursor.fetchone()
+
+                if record:
+                    url, title, tags, description, is_processed = record
+                    result_dict = {'url': url, 'title': title, 'tags': tags, 'description': description, 'is_processed': is_processed}
+                    print("Fetched record:", result_dict)
+                    return result_dict
+
+                else:
+                    print("No records with status 'NOT PROCESSED' found.")
+                    return None
+
+    except Exception as e:
+        # Handle any exceptions
+        print(f"Error: {e}")
+        return None
+
+def update_is_processed(url, new_status='DOWNLOADING'):
+    try:
+        # Connect to the database using the context manager
+        with connect_to_database() as connection:
+            # Create a cursor to interact with the database
+            with connection.cursor() as cursor:
+                # SQL UPDATE statement to set 'is_processed' to 'downloaded' based on exact URL match
+                update_query = """
+                    UPDATE public.video_metadata
+                    SET is_processed = %s
+                    WHERE url = %s;
+                """
+
+                # Execute the UPDATE query
+                cursor.execute(update_query, (new_status, url))
+
+                # Commit the changes to the database
+                connection.commit()
+
+                print(f"Updated 'is_processed' to '{new_status}' for URL '{url}'.")
+
+    except Exception as e:
+        # Handle any exceptions
+        print(f"Error: {e}")
+
+
+def update_user_video_count(email, video_count):
+    try:
+        # Connect to the database using the context manager
+        with connect_to_database() as connection:
+            # Create a cursor to interact with the database
+            with connection.cursor() as cursor:
+                # SQL UPDATE statement to set 'is_processed' to 'downloaded' based on exact URL match
+                update_query = """
+                    UPDATE public.accounts
+                    SET videos_uploaded = %s,
+                    in_use = False
+                    WHERE email = %s;
+                """
+
+                # Execute the UPDATE query
+                cursor.execute(update_query, (video_count, email))
+
+                # Commit the changes to the database
+                connection.commit()
+
+                print(f"Updated 'videos_uploaded' to '{video_count}' for email '{email}'.")
+
+    except Exception as e:
+        # Handle any exceptions
+        print(f"Error: {e}")
+
+def extract_file_id(google_drive_link):
+    link_parts = google_drive_link.split('/')
+    # Extract the file ID from the next part in the link
+    file_id = link_parts[-2]
+
+    return file_id
 
 # open chrome browser
 # Chrome browsers for Testing - c
@@ -49,7 +226,7 @@ def open_browser(chrome_binary_path="/Applications/Google Chrome.app/Contents/Ma
     chrome_service = ChromeService(ChromeDriverManager(driver_version=driver_version).install())
     
     chrome_options = uc.ChromeOptions()
-    # chrome_options.binary_location = chrome_binary_path
+    chrome_options.binary_location = chrome_binary_path
     
     chrome_options.add_argument("--window-size=1920,1080")
     #chrome_options.add_argument("--start-minimized")  # Add this line to start in minimized mode
@@ -77,6 +254,36 @@ def open_browser(chrome_binary_path="/Applications/Google Chrome.app/Contents/Ma
     return driver
 
 
+def open_remote_browser(grid_url, headless=False, user_agent=None, proxy=None, download_directory=None):
+    chrome_service = ChromeService(ChromeDriverManager().install())
+    
+    chrome_options = uc.ChromeOptions()
+    # chrome_options.binary_location = chrome_binary_path
+    
+    chrome_options.add_argument("--window-size=1920,1080")
+    #chrome_options.add_argument("--start-minimized")  # Add this line to start in minimized mode
+    chrome_options.add_argument("--disable-extensions") # Disable Chrome extensions
+    #chrome_options.add_argument('--disable-notifications') # Disable Chrome notifications
+    chrome_options.add_argument("--mute-audio") # Mute system audio
+    #chrome_options.add_argument('--disable-dev-shm-usage') # Disable the use of /dev/shm to store temporary data
+    #chrome_options.add_argument('--ignore-certificate-errors') # Ignore certificate errors
+    chrome_options.add_argument("--incognito")  # Start Chrome in incognito mode
+    #chrome_options.add_argument("--disable-geolocation")  # Disable geolocation in Chrome
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    if headless:
+        chrome_options.add_argument("--headless")
+    if user_agent:
+        #user_agent = UserAgent()['google chrome']
+        chrome_options.add_argument(f"--user-agent={user_agent}")
+    if proxy:
+        chrome_options.add_argument(f"--proxy-server={proxy}")
+    if download_directory:
+        preferences = {"download.default_directory": download_directory}
+        chrome_options.add_experimental_option("prefs", preferences)
+    
+    driver = webdriver.Remote(command_executor=grid_url, options=chrome_options)
+    
+    return driver
 
 # random time delay between requests (anti-blocking technique)
 def random_time_delay(start=10, end=20):
@@ -90,14 +297,12 @@ def load_credentials_df():
     return df.reset_index(drop=True)
 
 
-
 def load_video_df():
     df = pd.read_csv(PATH_DICT['VIDEO_METADATA_FILE'])
     df['filename'] = df['url'].apply(lambda x: x.split("/")[-2])
     df['tags'] = df['tags'].apply(lambda x: ", ".join(x.split(", ")[:10]))
     df['download_status'] = False
     return df.reset_index(drop=True)
-
 
 
 def download_video(gdrive_file_url, output_filename):
@@ -121,7 +326,6 @@ def download_video(gdrive_file_url, output_filename):
         return False
 
 
-
 def download_video_df(df):
     for i, row in tqdm(df.iterrows(), total=df.shape[0]):
         output_filepath = os.path.join(PATH_DICT['ORIGINAL_VIDEO_DIR'], row['filename']+'.mp4')
@@ -132,7 +336,6 @@ def download_video_df(df):
             df.at[i, 'download_status'] = True
         
     return df.reset_index(drop=True)
-
 
 
 def generate_task_df(df_account, df_video, n=3):
@@ -157,14 +360,12 @@ def generate_task_df(df_account, df_video, n=3):
     return df.reset_index(drop=True)
 
 
-
 def go_to_homepage():
     # go to homepage
     driver.get("https://www.youtube.com/")
    
     # wait to load
     random_time_delay(min_wait_time, max_wait_time)
-
 
 
 def login(email_id, password):
@@ -234,8 +435,6 @@ def go_to_upload_page():
         print("Open upload page failed - ", str(e))
         return False
 
-
-
 def select_file(video_path):
     try:
         absolute_path = os.path.abspath(video_path)
@@ -258,8 +457,6 @@ def select_file(video_path):
     except Exception as e:
         print("File selection failed - ", str(e))
         return False
-
-
 
 def upload_video(title, description, tags):
     try:
@@ -326,8 +523,6 @@ def upload_video(title, description, tags):
         print("Video upload failed - ", str(e))
         return False
 
-
-
 def logout():
     try:
         # go to homepage
@@ -358,79 +553,88 @@ if __name__=='__main__':
     for key, val in PATH_DICT.items():
         if 'DIR' in key:
             os.makedirs(val, exist_ok=True)
-        
-    df_account = load_credentials_df()
-    df_video = load_video_df()
-    df_video = download_video_df(df_video)
-    df_task = generate_task_df(df_account, df_video, n = videos_per_account)
+
+
+    # df_video = download_video_df(df_video)
+    # df_task = generate_task_df(df_account, df_video, n = videos_per_account)
     
-    df_task.to_csv(PATH_DICT['OUTPUT_FILE'], index=False)
+    # df_task.to_csv(PATH_DICT['OUTPUT_FILE'], index=False)
     
     #################### Pipeline execution ####################
     nerrors = 0
     current_login = ''
-    
-    for i, row in tqdm(df_task.iterrows(), total = df_task.shape[0]):
+    file_path = None
+    while True:
         try:
-            # If encountered a new account, then try login again with new account
-            if row['email_id'] != current_login:
-                # logout from old account (if driver open)
-                logout_status = logout()
-                
-                # close existing brower (if any)
-                try:
-                    driver.quit()
-                except:
-                    pass
-                
-                # open browser again
-                driver = open_browser(chrome_binary_path = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-                                    driver_version = '120.0.6099.234',
-                                    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36')
-                
-                # try login - can be successful or failed
-                login_status = login(row['email_id'], row['password'])
-                
-                # update current login account
-                current_login = row['email_id']
-                
-            # If encountered account before, then
+            user_info = get_and_update_user()
+            video_info = fetch_video()
+            if user_info is None:
+                print("No user available")
+            elif video_info is None:
+                print("No video available")
             else:
-                # If login to account was successful, go to homepage
-                if login_status:
-                    go_to_homepage()
-                    
-                # If login to account was failed, then skip task
+                email = user_info['email']
+                password = user_info['password']
+                user_videos_uploaded = user_info['videos_uploaded']
+                title = video_info['title']
+                description = video_info['description']
+                tags = video_info['tags']
+                is_processed = video_info['is_processed']
+                video_url = video_info['url']
+                file_name = extract_file_id(video_url)
+                output_filepath = os.path.join(PATH_DICT['ORIGINAL_VIDEO_DIR'], file_name+'.mp4')
+                file_path = output_filepath
+                # Update it to downloading
+                update_is_processed(video_url)
+                # download_video(video_url, output_filepath)
+                if email != current_login:
+                    logout_status = logout()
+                    try:
+                        driver.quit()
+                    except:
+                        pass
+
+                    driver = open_browser(chrome_binary_path = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+                            driver_version = '120.0.6099.234',
+                            user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36')
+                        
+                    # try login - can be successful or failed
+                    login_status = login(email, password)
+                    current_login = email
+
+                # If encountered account before, then
                 else:
-                    continue
-            
-            # If login to current account successful, then proceed to upload
-            if login_status:
-                go_to_upload_page_status = go_to_upload_page()
-                
-                select_file_status = select_file(os.path.join(PATH_DICT['ORIGINAL_VIDEO_DIR'], row['filename']+'.mp4'))
-    
-                upload_status = upload_video(title=row['title'], description=row['description'], tags=row['tags'])
-                
-                df_task.at[i, 'login_status'] = login_status
-                df_task.at[i, 'go_to_upload_page_status'] = go_to_upload_page_status
-                df_task.at[i, 'select_file_status'] = select_file_status
-                df_task.at[i, 'upload_status'] = upload_status
-                
-                df_task.to_csv(PATH_DICT['OUTPUT_FILE'], index=False)
-                
-            print(current_login)
-            
+                    # If login to account was successful, go to homepage
+                    if login_status:
+                        go_to_homepage()
+
+                    # If login to account was failed, then skip task
+                    else:
+                        continue
+
+                # If login to current account successful, then proceed to upload
+                if login_status:
+                    go_to_upload_page_status = go_to_upload_page()
+                    
+                    select_file_status = select_file(os.path.join(PATH_DICT['ORIGINAL_VIDEO_DIR'], file_name+'.mp4'))
+
+                    upload_status = upload_video(title=title, description=description, tags=tags)
+                    if upload_status:
+                        update_is_processed(video_url,'DONE')
+                        update_user_video_count(email,user_videos_uploaded+1)
+                    else:
+                        update_is_processed(video_url,'NOT PROCESSED')
+                        update_user_video_count(email,user_videos_uploaded)
+                else:
+                    update_is_processed(video_url,'NOT PROCESSED')
+                    update_user_video_count(email,user_videos_uploaded)
+
         except Exception as e:
-            print("Error in pipeline -", str(e))
-            nerrors += 1
-            if nerrors == 3:
-                break
             pass
-    
-    
-    # close existing brower (if any)
-    try:
-        driver.quit()
-    except:
-        pass
+        finally:
+            try:
+                os.remove(file_path)
+                print(f"File '{file_path}' deleted successfully.")
+            except OSError as e:
+                print(f"Error deleting file '{file_path}': {e}")
+            random_time_delay(min_wait_time, max_wait_time)
